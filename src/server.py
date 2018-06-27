f12b9852769119b302c0f3e128d2ba9ae25178f7
Rakshit Ramesh <rakshitadmar@gmail.com>
2018-06-28 00:16:41 +0530

    Add data integrations

--- src/server.py
+++ src/server.py
@@ -5,16 +5,17 @@
     API's
 
     RegisterApp :- {"AppName":"App1","MaxStreetlights":10,
-                        "Policy":<AutoTimer>|<Manual>|<AutoLux>, "Lights" = [123,234]}
+                        "Policy":"<AutoTimer>|<Manual>|<AutoLux>", "PolicyParams":<PolicyParams>, "Lights" = [123,234]}
 
                                 
-        Policy Object Expansion :- {"AutoTimer":["hour":18,"minute":30,"brightness":50]} # Time in 24hours format 
-                                   {"Manual":{"Brightness":100}} # Integer Value less than 100
-                                   {"AutoLux":{"OnThreshold":1,"OffThreshold":2}} # Integer 
+        Policy Params Expansion :- AutoTimer     ["hour":18,"minute":30,"brightness":50]} # Time in 24hours format 
+                                   Manual        {"Brightness":100}} # Integer Value less than 100
+                                   AutoLux       {"OnThreshold":1,"OffThreshold":2}} # Integer 
 
     RegisterLight :- {"AppName":"App1","Light":70b3d58ff0031de5}
 
 
+    appendPolicy :- {"AppName":"App1","Policy": "AutoTimer", "PolicyParams": {[{"hour":12,"minute":4,"brightness":0}]}}
 
     Hierarchy in MongoDB :- streetlightsDB -> <applicationID> -> <streetlightId>
                             (Collection)
@@ -43,7 +44,7 @@ workingDir = sys.path[0]
 items = {}
 
 context = zmq.Context()
-print("Connecting to Adapter with ports %s" % 4725)
+print("Connecting to socket with ports %s" % 4725)
 socket = context.socket(zmq.PUB)
 socket.connect("tcp://localhost:%s" % 4725)
 
@@ -57,17 +58,34 @@ class registerApp(Resource):
 
         socket.send_string("streetlights" + '$' + "newApp" + '$' + json.dumps(json_data))
 
+class appendPolicy(Resource):
+    def post(self):
+        json_data = request.get_json()
+        print(json_data)
+        if("AutoTimer" in json_data["Policy"]):
+            streetlights.update({"AppName": json_data["AppName"]},{"$push":{"PolicyParams":json_data["PolicyParams"][0]}})
+        socket.send_string("streetlights" + '$' + "appendPolicy" + '$' + json.dumps(json_data))
 
 class registerLight(Resource):
     def post(self):
         json_data = request.get_json()
-        lightId = json_data["light"]
+        lights = json_data["Lights"]
         appName = json_data["AppName"]
-        db.streetlights.update({"AppName": appName}, {"$push": {"Lights": lightId}})
+        for light in lights: 
+            streetlights.update({"AppName": appName}, {"$push": {"Lights": light}})
         socket.send_string("streetlights" + '$' + "newLight" + '$' + json.dumps(json_data))
+
+# To be filled
+#class deletePolicy(Resource):
+#    def delete():
 
 api.add_resource(registerApp, '/registerApp')
 api.add_resource(registerLight, '/registerLight')
+api.add_resource(appendPolicy, '/appendPolicy')
+#To be Filled
+#api.add_resource(deleteApp, '/deleteApp')
+#api.add_resource(deleteLight, '/deleteLight')
+#api.add_resource(deletePolicy, '/deletePolicy')
 
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1")
